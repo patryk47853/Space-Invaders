280657ce2893a86d391e76c44add4fe12f0fbc88
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-12-23 11:09:57 +0100

    Create Alien entity

--- core/src/main/java/pl/patrykjava/game/Alien.java
+++ core/src/main/java/pl/patrykjava/game/Alien.java
@@ -1,4 +1,24 @@
 package pl.patrykjava.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
 public class Alien {
+
+    public Vector2 position;
+    public boolean alive = true;
+    public Sprite sprite;
+
+    public Alien(Vector2 position, Texture img) {
+        this.position = position;
+        sprite = new Sprite(img);
+        sprite.setScale(4);
+    }
+
+    public void Draw(SpriteBatch batch) {
+        sprite.setPosition(position.x, position.y);
+        sprite.draw(batch);
+    }
 }
